3bafddc5b683b66be85d5ec848dbb6b4ea1fd853
AJP-432 <58121633+AJP-432@users.noreply.github.com>
2024-03-13 11:44:19 -0400

    edit: update resume link, header change

--- pages/components/About.tsx
+++ pages/components/About.tsx
@@ -25,7 +25,7 @@ const About = () => {
             <span>{about.secondary}</span>
           </div>
           <div className="mt-6 mx-auto md:mx-0 border-2 border-white py-1 px-2 w-36 text-center font-bold">
-            <a href={'https://drive.google.com/file/d/1l5wvbFmVun9n8hICKouDsuBmCgLRWPPs/view?usp=sharing'} className="about__resume text-white text-l" target="_blank">View Resume</a>
+            <a href={'https://drive.google.com/drive/folders/1wCFiKkEMS1iqkEpG9EQlIAOt0wQdm9ZO?usp=sharing'} className="about__resume text-white text-l" target="_blank">View Resume</a>
           </div>
         </div>
       </div>
